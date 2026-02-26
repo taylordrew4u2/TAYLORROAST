f5e9f9dc050eba53382ef02f64382c969a2a2379
taylordrew4u2 <taylordrew4u@gmail.com>
2026-02-26 07:15:14 +0000

    fix: correct syntax in postcss.config.js and add Turso connection settings

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: {
     "@tailwindcss/postcss": {},
-  },
+  }TURSO_AUTH_TOKEN="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NzIwNDY5MDUsImlkIjoiMDE5Yzk2MzktZjAwMS03MmM5LWE5NTItZmE0ZDE2MGQ4NjZlIiwicmlkIjoiNTE1ZGUwNjAtNzVmZC00MjQ4LTg4MDItNGZjOGYwODIwNGI0In0.FvhuE1qwXDkt7rLzSRg2YNkHKS5yidtS4OirvZJe21TDCUlqT9M9DN_kj_l3QPtXm2HuK8-63aGbNVNSWgipCw"
+  TURSO_DATABASE_URL="libsql://database-pink-apple-vercel-icfg-bcu7zzamdox9nbdpkugsrnzd.aws-us-east-1.turso.io",
 };
